1f2b548b5f26f78d0e4ad358c0150f3105425068
RoshanKumarJha18 <roshanjhajha76@gmail.com>
2026-05-01 12:26:03 +0530

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
 }

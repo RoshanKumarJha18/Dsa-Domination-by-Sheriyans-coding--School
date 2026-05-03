969338005fd072ee4b72153301af2a7e6311ec96
RoshanKumarJha18 <roshanjhajha76@gmail.com>
2026-05-03 13:11:28 +0530

    second commit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        System.out.println("enter the value for n ");
+        int n = sc.nextInt();
+        //outer loop
+        for (int i = 1; i <= n ; i++) {
+            //inner loop
+            for (int j = 1; j <= n ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
